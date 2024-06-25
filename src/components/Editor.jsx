09c7ffc693c9159138f13e17e01ecd02c068e1a4
avinash1606 <avinashkumarmehta2002@gmail.com>
2024-06-25 22:55:21 +0530

    first commit

---
+++ src/components/Editor.jsx
@@ -0,0 +1,55 @@
+import  { useState } from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/material.css'
+import 'codemirror/mode/xml/xml'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/css/css'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
+
+export default function Editor(props) {
+   
+    const {
+         /* eslint-disable-next-line */
+    language,
+    /* eslint-disable-next-line */
+    displayName,
+    /* eslint-disable-next-line */
+    value,
+    /* eslint-disable-next-line */
+    onChange
+  } = props
+  const [open, setOpen] = useState(true)
+
+  function handleChange(editor, data, value) {
+    onChange(value)
+  }
+
+  return (
+    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
+      <div className="editor-title">
+        {displayName}
+        <button
+          type="button"
+          className="expand-collapse-btn"
+          onClick={() => setOpen(prevOpen => !prevOpen)}
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          lint: true,
+          mode: language,
+          theme: 'material',
+          lineNumbers: true
+        }}
+      />
+    </div>
+  )
+}
